1b9cf9da1e33616d0356dae4135bc370b3b22f30
Reda Lemeden <git@redalemeden.com>
2020-04-08 00:05:06 +0200

    Add NIO exports

---
+++ Sources/Glide/Exports.swift
@@ -0,0 +1,20 @@
+@_exported import Foundation
+
+@_exported import struct NIOHTTP1.HTTPHeaders
+@_exported import enum NIOHTTP1.HTTPMethod
+@_exported import struct NIOHTTP1.HTTPVersion
+@_exported import enum NIOHTTP1.HTTPResponseStatus
+
+@_exported import struct NIO.ByteBuffer
+@_exported import struct NIO.ByteBufferAllocator
+@_exported import protocol NIO.Channel
+@_exported import class NIO.EmbeddedChannel
+@_exported import class NIO.EmbeddedEventLoop
+@_exported import protocol NIO.EventLoop
+@_exported import class NIO.EventLoopFuture
+@_exported import protocol NIO.EventLoopGroup
+@_exported import struct NIO.EventLoopPromise
+@_exported import class NIO.MultiThreadedEventLoopGroup
+@_exported import struct NIO.NonBlockingFileIO
+@_exported import class NIO.NIOThreadPool
+@_exported import enum NIO.System
