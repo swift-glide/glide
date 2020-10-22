f58a09a2bde8c5a661d1e6c14d4cdd296ccfc6b7
Reda Lemeden <git@redalemeden.com>
2020-10-22 23:01:00 +0200

    Move aliases to the correct file

--- Sources/Glide/Core/Aliases.swift
+++ Sources/Glide/Core/Aliases.swift
@@ -2,3 +2,12 @@ import Foundation
 import NIO
 
 public typealias Future = EventLoopFuture
+
+public typealias Handler = () -> Future<Void>
+public typealias Middleware = (Request, Response) throws -> Future<MiddlewareOutput>
+
+public typealias ThrowingSyncHTTPHandler = (Request, Response) throws -> Void
+public typealias SyncHTTPHandler = (Request, Response) -> Void
+
+public typealias ErrorHandler = ([Error], Request, Response) -> Future<Void>
+public typealias SyncErrorHandler = ([Error], Request, Response) -> Void
--- Sources/Glide/Middleware/Middleware.swift
+++ Sources/Glide/Middleware/Middleware.swift
@@ -1,11 +1,6 @@
 import Foundation
 import NIO
 
-public typealias Handler = () -> Future<Void>
-public typealias Middleware = (Request, Response) throws -> Future<MiddlewareOutput>
-public typealias SyncHTTPHandler = (Request, Response) throws -> Void
-public typealias ErrorHandler = ([Error], Request, Response) -> Future<Void>
-
 public enum MiddlewareOutput {
   case next
   case send(String, as: MIMEType = .plainText)
@@ -21,7 +16,7 @@ public enum MiddlewareOutput {
   }
 }
 
-public func passthrough(_ perform: @escaping SyncHTTPHandler) -> Middleware {
+public func passthrough(_ perform: @escaping ThrowingSyncHTTPHandler) -> Middleware {
   return { request, response in
     try perform(request, response)
     return request.next()
